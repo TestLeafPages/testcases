7422f93c20668593a1fba46c44208e477ec685ff
TestLeafPages <earth@testleaf.com>
2023-01-19 18:47:25 +0530

    Update EditLead.java

--- testcase/EditLead.java
+++ testcase/EditLead.java
@@ -5,12 +5,12 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class EditLead {
 
 	public static void main(String[] args) throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
+		
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://leaftaps.com/opentaps/");
