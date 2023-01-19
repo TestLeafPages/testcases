fa36b8493439207b6d47ec1577b81fd47f4f8436
TestLeafPages <earth@testleaf.com>
2023-01-19 18:49:11 +0530

    Update MergeLead.java

--- testcase/MergeLead.java
+++ testcase/MergeLead.java
@@ -8,12 +8,12 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class MergeLead {
 
 	public static void main(String[] args) throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
+		
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://leaftaps.com/opentaps/");
